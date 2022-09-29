d59fa095e4c09d04841cd3338cfbabedbc445f53
Caleb Hayworth <chayworth1392@gmail.com>
2022-09-29 12:56:20 -0400

    Updates to styles

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <banner-view id="banner" />
     <body>
-      <carousel id="carousel" />
       <search-bar id="search" />
       <side-bar id="navbar" />
+      <carousel id="carousel" />
       <div id="bg">
         <img
           src="https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
