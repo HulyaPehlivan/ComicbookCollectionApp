79d265f4f99ad243c807e0e07418306d46f2cd45
Caleb Hayworth <chayworth1392@gmail.com>
2022-10-01 15:38:22 -0400

    avoiding merge conflicts

--- capstone/vue/src/services/ComicService.js
+++ capstone/vue/src/services/ComicService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
     getAllComics() {
         return axios.get("/comics")
-    }, 
+    },
 
     getComicById(apiID) {
         return axios.get(`/issues/${apiID}`)
