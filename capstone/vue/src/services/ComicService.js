04906de5d281787a12c045e44126210519c5e5aa
Caleb Hayworth <chayworth1392@gmail.com>
2022-10-04 11:01:31 -0400

    Styles for collection carousel, methods for setting current collection

--- capstone/vue/src/services/ComicService.js
+++ capstone/vue/src/services/ComicService.js
@@ -19,5 +19,12 @@ export default {
     },
     getComicByStoryArc(name) {
         return axios.get(`/story_arc/${name}`)
+    },
+    getComicByCollectionId(collectionId) {
+        return axios.get(`/comics/collectionId/${collectionId}`)
     }
 }
+// ComicService.getAllComics().then((response) => {
+//     console.log(response.data);
+//     this.comics = response.data;
+//     this.$store.commit("LOAD_ALL_COMICS", this.comics);
